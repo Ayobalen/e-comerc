58463a4e5f0c0abf8d5149c539fead78bc010b60
Balen <idrisrasheed2000@gmail.com>
2022-07-19 16:38:32 +0100

    update

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const BASE_URL = "http://localhost:5000/api/";
-const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyYmUwNDEyYzA4YmIzNWY2MzY3MGU4YyIsImlhdCI6MTY1Njk4MDMyMCwiZXhwIjoxNjU3MjM5NTIwfQ.FjOrGnhXPe2Ee6FC3OFYbTvSQWtZJFNCiHXAK3vmjWg";
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem('persist:root')).user)
+.currentUser.accessToken;
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL,
